f2f3822780374fc1806d54619fe36c4235fa43d3
daniel <riyueniao2010@gmail.com>
2024-07-03 00:52:56 +0800

    feat: update

--- user.py
+++ user.py
@@ -12,7 +12,9 @@ def reset():
 
 
 def query_user(name):
-    data = Users.select().where(Users.name == name)[0]
+    data = Users.select().where(Users.name == name)
+    print("aaa", name, len(data))
+    data = data[0]
     user = User(data.id, data.name, data.money, data.status)
     return user
 

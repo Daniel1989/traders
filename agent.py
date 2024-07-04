49cde0b45a2e66e4bdea57dc521666720bf8741b
daniel <riyueniao2010@gmail.com>
2024-07-04 23:38:31 +0800

    feat: optimize data handler

--- agent.py
+++ agent.py
@@ -33,13 +33,13 @@ class Agent:
             return user
         return query_user(name)
 
-    def handler(self):
+    def handler(self, analyze_data):
         if self.llm.is_chinese:
             name = "白银"
         else:
             name = "silver"
         try:
-            self.do_action(name)
+            self.do_action(name, analyze_data)
         except Exception as e:
             print(e)
             print(traceback.print_exc())
@@ -65,66 +65,14 @@ class Agent:
         plt.savefig('plot.png')
         plt.close()
 
-    def get_interval_statement(self, minute_history_data, history_daily_data):
-        minute_result, _ = calc_interval(minute_history_data[:-1], is_minute=True)
-        daily_result, _ = calc_interval(history_daily_data, is_minute=False)
-        p95_min = min(minute_result.iloc[:, 2].to_list())
-        p95_max = max(minute_result.iloc[:, 5].to_list())
-        p80_min = min(minute_result.iloc[:, 3].to_list())
-        p80_max = max(minute_result.iloc[:, 4].to_list())
-        s1 = ("基于近一年的每日收盘价格，通过使用Auto-Regressive Integrated Moving Average(ARIMA)模型对今日收盘价格进行预测，"
-              "得到80%的置性区间为：[" + str(daily_result.iloc[0][3]) + ", " + str(daily_result.iloc[0][4]) + "]。"
-               "95%的置性区间为：[" + str(daily_result.iloc[0][2]) + ", " + str(daily_result.iloc[0][5]) + "]。")
-
-        s2 = ("基于最近6个小时内的每分钟收盘价格，通过使用Auto-Regressive Integrated Moving Average(ARIMA)模型对接下去的一小时内的每分钟收盘价格进行预测，"
-              "得到结果如下，有80%的可能价格在[" + str(p80_min) + ", " + str(p80_max) + "]的区间内变动,"
-              "95%的的可能价格在[" + str(p95_min) + ", " + str(p95_max) + "]的区间内变动。")
-        return s1 + "同时，" + s2 + "\n"
-
-    def forecast_check(self, minute_history_data, history_daily_data):
-        today = minute_history_data[len(minute_history_data) - 1]
-        current_price = today[4]
-
-        minute_result, _ = calc_interval(minute_history_data[:-1], is_minute=True)
-        daily_result, _ = calc_interval(history_daily_data, is_minute=False)
-        p95_min = min(minute_result.iloc[:, 2].to_list())
-        p95_max = max(minute_result.iloc[:, 5].to_list())
-        p80_min = min(minute_result.iloc[:, 3].to_list())
-        p80_max = max(minute_result.iloc[:, 4].to_list())
-        content = '白银当前价格' + str(current_price)
-        need_send = True
-        if current_price <= daily_result.iloc[0][2]:
-            content += ",低于日线级别95%置性区间内的最低值" + str(daily_result.iloc[0][2])
-        elif current_price <= daily_result.iloc[0][3]:
-            content += ",低于日线级别80%置性区间内的最低值" + str(daily_result.iloc[0][3])
-        elif current_price >= daily_result.iloc[0][5]:
-            content += ",高于日线级别95%置性区间内的最大值" + str(daily_result.iloc[0][5])
-        elif current_price >= daily_result.iloc[0][4]:
-            content += ",高于日线级别80%置性区间内的最大值" + str(daily_result.iloc[0][4])
-        elif current_price <= p95_min:
-            content += ",低于分钟级别95%置性区间内的最低值" + str(p95_min)
-        elif current_price <= p80_min:
-            content += ",低于分钟级别80%置性区间内的最低值" + str(p80_min)
-        elif current_price >= p95_max:
-            content += ",高于分钟级别95%置性区间内的最大值" + str(p95_max)
-        elif current_price >= p80_max:
-            content += ",高于分钟级别80%置性区间内的最大值" + str(p80_max)
-        else:
-            need_send = False
-        content += "\n区间值详细信息如下:\n"
-        content += "日线80%区间: [" + str(daily_result.iloc[0][3]) + ", " + str(daily_result.iloc[0][4]) + "]\n"
-        content += "日线95%区间: [" + str(daily_result.iloc[0][2]) + ", " + str(daily_result.iloc[0][5]) + "]\n"
-        content += "分钟线80%区间: [" + str(p80_min) + ", " + str(p80_max) + "]\n"
-        content += "分钟线95%区间: [" + str(p95_min) + ", " + str(p95_max) + "]\n"
-
-        if need_send:
-            send_common_to_ding(content)
-
-    def analyze(self, minute_history_data, history_daily_data, daily, goods, max_shares_num,
+    def analyze(self, analyze_data, daily, goods, max_shares_num,
                 current_position=DEFAULT_POSITION,
                 total_profit=0):
-        daily_data = history_daily_data[-10:]
-        data = minute_history_data[-61:-1]
+        daily_history = analyze_data["daily_history"]
+        minute_history = analyze_data["minute_history"]
+        forecast_result = analyze_data["forecast_result"]
+        daily_data = daily_history[-10:]
+        data = minute_history[-61:-1]
         current_price = float(daily[4])
         current_loss = ''
         history_data = ''
@@ -133,7 +81,6 @@ class Agent:
             history_data += f"date: {item[0]}, open: {item[1]}, high: {item[2]}, low: {item[3]}, close: {item[4]}, volume: {item[5]}\n"
         for item in daily_data:
             daily_history_data += f"date: {item[0]}, open: {item[1]}, high: {item[2]}, low: {item[3]}, close: {item[4]}, volume: {item[5]}\n"
-        forecast_result = self.get_interval_statement(minute_history_data, history_daily_data)
         if self.llm.is_chinese:
             if total_profit != 0:
                 current_loss = "当前持仓已经产生了" + str(total_profit) + "元的" + (
@@ -162,7 +109,6 @@ class Agent:
 
         prompt = generate_prompt(curr_input, prompt_lib_file)
         res = self.llm.do_prompt(prompt)
-
         try:
             matches = re.findall(r'\{([^{}]*)\}', res)
             cleaned_json_string = re.sub(r'//.*', '', matches[0])
@@ -176,19 +122,11 @@ class Agent:
             print(error)
             raise error
 
-    def do_action(self, goods):
-        try:
-            url = (
-                "http://stock2.finance.sina.com.cn/futures/api/json.php/IndexService.getInnerFuturesDailyKLine?symbol"
-                "=ag2408")
-            daily_history = request(url)
-        except Exception as e:
-            print(e)
-            return
-        minute_history = get_goods_minute_data('AG2408')
+    def do_action(self, goods, analyze_data):
+        minute_history = analyze_data["minute_history"]
         # 这里插一个钉钉通知
-        if self.modal_name == 'doubao':
-            self.forecast_check(minute_history, daily_history)
+        # if self.modal_name == 'doubao':
+        #     self.forecast_check(minute_history, daily_history)
 
         current_item = minute_history[len(minute_history) - 1]
         current_price = float(current_item[4])
@@ -267,7 +205,7 @@ class Agent:
                     print("买一手的钱都没有了:(")
                     # TODO 这里需要更新用户状态为死亡
                     return
-            action = self.analyze(minute_history, daily_history, current_item, goods, max_shares_num, current_position,
+            action = self.analyze(analyze_data, current_item, goods, max_shares_num, current_position,
                                   total_profit)
             user.action(action, goods, current_price)
         except Exception as e:
--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from agent import Agent
 import datetime
 import uuid
 from concurrent.futures import ProcessPoolExecutor, TimeoutError
+
+from service.fpp3 import calc_interval
+from service.futures_data import get_goods_minute_data
+from util.cache import request
+from util.notify import send_common_to_ding
 from util.utils import is_trade_time, is_sync_time
 
 # from llm.openai_gpt import OpenaiModel
@@ -43,7 +48,7 @@ def item_exists(data_list, field, value):
     return any(item[field] == value for item in data_list)
 
 
-def execution(startup_uid, agent_name):
+def execution(startup_uid, agent_name, analyze_data):
     llm = [item for item in llm_list if item.model_name == agent_name][0]
     agent_list = Startup.select().where(Startup.uid == startup_uid, Startup.model_name == llm.model_name)
     print(agent_name, len(agent_list))
@@ -54,12 +59,64 @@ def execution(startup_uid, agent_name):
         agent = Agent(llm)
         startup_agent = Startup(uid=startup_uid, model_name=llm.model_name, agent_name=agent.name)
         startup_agent.save()
-    return agent.handler()
+    return agent.handler(analyze_data)
+
+
+def forecast_check(minute_history_data, history_daily_data):
+    today = minute_history_data[len(minute_history_data) - 1]
+    current_price = today[4]
+
+    minute_result, _ = calc_interval(minute_history_data[:-1], is_minute=True)
+    daily_result, _ = calc_interval(history_daily_data, is_minute=False)
+    p95_min = min(minute_result.iloc[:, 2].to_list())
+    p95_max = max(minute_result.iloc[:, 5].to_list())
+    p80_min = min(minute_result.iloc[:, 3].to_list())
+    p80_max = max(minute_result.iloc[:, 4].to_list())
+    content = '白银当前价格' + str(current_price)
+    need_send = True
+    if current_price <= daily_result.iloc[0][2]:
+        content += ",低于日线级别95%置性区间内的最低值" + str(daily_result.iloc[0][2])
+    elif current_price <= daily_result.iloc[0][3]:
+        content += ",低于日线级别80%置性区间内的最低值" + str(daily_result.iloc[0][3])
+    elif current_price >= daily_result.iloc[0][5]:
+        content += ",高于日线级别95%置性区间内的最大值" + str(daily_result.iloc[0][5])
+    elif current_price >= daily_result.iloc[0][4]:
+        content += ",高于日线级别80%置性区间内的最大值" + str(daily_result.iloc[0][4])
+    elif current_price <= p95_min:
+        content += ",低于分钟级别95%置性区间内的最低值" + str(p95_min)
+    elif current_price <= p80_min:
+        content += ",低于分钟级别80%置性区间内的最低值" + str(p80_min)
+    elif current_price >= p95_max:
+        content += ",高于分钟级别95%置性区间内的最大值" + str(p95_max)
+    elif current_price >= p80_max:
+        content += ",高于分钟级别80%置性区间内的最大值" + str(p80_max)
+    else:
+        need_send = False
+    content += "\n区间值详细信息如下:\n"
+    content += "日线80%区间: [" + str(daily_result.iloc[0][3]) + ", " + str(daily_result.iloc[0][4]) + "]\n"
+    content += "日线95%区间: [" + str(daily_result.iloc[0][2]) + ", " + str(daily_result.iloc[0][5]) + "]\n"
+    content += "分钟线80%区间: [" + str(p80_min) + ", " + str(p80_max) + "]\n"
+    content += "分钟线95%区间: [" + str(p95_min) + ", " + str(p95_max) + "]\n"
+
+    if need_send:
+        send_common_to_ding(content)
+
+    s1 = ("基于近一年的每日收盘价格，通过使用Auto-Regressive Integrated Moving Average(ARIMA)模型对今日收盘价格进行预测，"
+          "得到80%的置性区间为：[" + str(daily_result.iloc[0][3]) + ", " + str(daily_result.iloc[0][4]) + "]。"
+                                                                                                         "95%的置性区间为：[" + str(
+        daily_result.iloc[0][2]) + ", " + str(daily_result.iloc[0][5]) + "]。")
+
+    s2 = ("基于最近6个小时内的每分钟收盘价格，通过使用Auto-Regressive Integrated Moving Average(ARIMA)模型对接下去的一小时内的每分钟收盘价格进行预测，"
+          "得到结果如下，有80%的可能价格在[" + str(p80_min) + ", " + str(p80_max) + "]的区间内变动,"
+                                                                                   "95%的的可能价格在[" + str(
+        p95_min) + ", " + str(p95_max) + "]的区间内变动。")
+    return s1 + "同时，" + s2 + "\n"
 
 
 if __name__ == '__main__':
     # reset()
-    startup_id = str(uuid.uuid4())
+    # startup_id = str(uuid.uuid4())
+    startup_id = "4a283c5d-3f43-42ca-a369-69a918e74ce9"
     results = []
     while True:
         today = datetime.date.today()
@@ -72,9 +129,23 @@ if __name__ == '__main__':
             time.sleep(5 * 60)
             continue
 
+        try:
+            url = (
+                "http://stock2.finance.sina.com.cn/futures/api/json.php/IndexService.getInnerFuturesDailyKLine?symbol"
+                "=ag2408")
+            daily_history = request(url)
+        except Exception as e:
+            print(e)
+            continue
+
+        minute_history = get_goods_minute_data('AG2408')
+        forecast_result = forecast_check(minute_history, daily_history)
         with ProcessPoolExecutor() as executor:
-            futures = [executor.submit(execution, startup_id, item.model_name) for
-                       item in llm_list if item.model_name != 'llama3']
+            futures = [executor.submit(execution, startup_id, item.model_name, {
+                "daily_history": daily_history,
+                "minute_history": minute_history,
+                "forecast_result": forecast_result
+            }) for item in llm_list if item.model_name != 'llama3']
             for future in futures:
                 try:
                     result = future.result(timeout=10000000)  # Wait for all processes to complete

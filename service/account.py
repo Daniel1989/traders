98396f7a0b2240e019609148abf6741324b05016
三山 <sanshan@ywwl.com>
2024-07-09 17:26:01 +0800

    fix: add empty users

--- service/account.py
+++ service/account.py
@@ -11,16 +11,21 @@ def query_goods_current_price(goods_name):
 
 
 def daily_count():
-    query = Userstatus.select(Userstatus.volume, Users.money, Userstatus.goods, Users.name).where(
-        Userstatus.close.is_null()).join(Users, on=(Users.id == Userstatus.user_id))
+    query = Users.select(Userstatus.volume, Users.money, Userstatus.goods, Users.name).left_outer_join(Userstatus, on=(Users.id == Userstatus.user_id) & (Userstatus.close.is_null()))
     # 获取每个资产收盘价格
     # 将资产*收盘价格*数量 + 账户资产
     users = {}
+    # cur = database.cursor()
+    # print(cur.mogrify(*query.sql()))
+    # print(len(query))
     for item in query:
-        if item.users.name not in users:
-            users[item.users.name] = item.users.money
-        goods_price = query_goods_current_price(item.goods)
-        users[item.users.name] += goods_price.current_price * item.volume
+        if item.name not in users:
+            users[item.name] = item.money
+        try:
+            goods_price = query_goods_current_price(item.userstatus.goods)
+            users[item.name] += goods_price.current_price * item.userstatus.volume
+        except Exception as e:
+            print(item.name + str(e))
     for key in users:
         today = datetime.datetime.now().strftime('%Y-%m-%d')
         today_record = DailyAccount.select().where(DailyAccount.account_name == key, DailyAccount.date == today)
@@ -29,6 +34,4 @@ def daily_count():
         else:
             daily = DailyAccount(account_name=key, value=users[key], date=today)
             daily.save()
-    # cur = database.cursor()
-    # print(cur.mogrify(*query.sql()))
-    # print(len(query))
+
